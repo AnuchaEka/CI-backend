dacd7d0074f51ff818635ff86b03cb6de69eedeb
Anucha Aeka <anucha_cs@hotmail.com>
2017-09-08 11:15:51 +0700

    update code

--- application/views/mt-admin/pages/pagesform.blade.php
+++ application/views/mt-admin/pages/pagesform.blade.php
@@ -1,9 +1,5 @@
 @extends('mt-admin.master') 
 
-@section('css')
- <link href="{{base_url('assets/pages/css/blog.css')}}" rel="stylesheet" type="text/css" /> 
-@endsection
-
 @section('content')
 <div class="row">
     <div class="col-md-12">
@@ -12,15 +8,15 @@
                 <div class="portlet-title">
                     <div class="caption">
                         <i class="icon-doc"></i>{{$title}} </div>
-                        @if(sizeof($res)>0)
-                        <div class="actions btn-set">
-                                 
-                                    <a href="{{base_url('mt-admin/'.$uri->segment(2).'/add')}}" class="btn default btn-secondary-outline">
+                    @if(sizeof($res)>0)
+                    <div class="actions btn-set">
+
+                        <a href="{{base_url('mt-admin/'.$uri->segment(2).'/add')}}" class="btn default btn-secondary-outline">
                                         {{$web->getLable('add')}} <i class="fa fa-plus"></i></a>
-                            
-                                   
-                       </div>    
-                       @endif
+
+
+                    </div>
+                    @endif
 
                 </div>
                 <div class="portlet-body">
@@ -34,76 +30,133 @@
                     @endif
 
                     <div class="portlet-body">
-                    <div class="panel-group accordion" id="accordion3">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title">
-                                    <a class="accordion-toggle accordion-toggle-styled" data-toggle="collapse" data-parent="#accordion3" href="#collapse_3_1"> Collapsible Group Item #1 </a>
-                                </h4>
-                            </div>
-                            <div id="collapse_3_1" class="panel-collapse in">
-                                <div class="panel-body">
-                                    <p> Duis autem vel eum iriure dolor in hendrerit in vulputate. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut. </p>
-                                    <p> Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                                        </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title">
-                                    <a class="accordion-toggle accordion-toggle-styled collapsed" data-toggle="collapse" data-parent="#accordion3" href="#collapse_3_2"> Collapsible Group Item #2 </a>
-                                </h4>
-                            </div>
-                            <div id="collapse_3_2" class="panel-collapse collapse">
-                                <div class="panel-body" style="height:200px; overflow-y:auto;">
-                                    <p> Duis autem vel eum iriure dolor in hendrerit in vulputate. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut. </p>
-                                    <p> Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                                        </p>
-                                    <p> Duis autem vel eum iriure dolor in hendrerit in vulputate. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut. </p>
-                                    <p>
-                                        <a class="btn blue" href="ui_tabs_accordions_navs.html#collapse_3_2" target="_blank"> Activate this section via URL </a>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title">
-                                    <a class="accordion-toggle accordion-toggle-styled collapsed" data-toggle="collapse" data-parent="#accordion3" href="#collapse_3_3"> Collapsible Group Item #3 </a>
-                                </h4>
-                            </div>
-                            <div id="collapse_3_3" class="panel-collapse collapse">
-                                <div class="panel-body">
-                                    <p> Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                                        Brunch 3 wolf moon tempor. </p>
-                                    <p> Duis autem vel eum iriure dolor in hendrerit in vulputate. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut. </p>
-                                    <p>
-                                        <a class="btn green" href="ui_tabs_accordions_navs.html#collapse_3_3" target="_blank"> Activate this section via URL </a>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title">
-                                    <a class="accordion-toggle accordion-toggle-styled collapsed" data-toggle="collapse" data-parent="#accordion3" href="#collapse_3_4"> Collapsible Group Item #4 </a>
-                                </h4>
-                            </div>
-                            <div id="collapse_3_4" class="panel-collapse collapse">
-                                <div class="panel-body">
-                                    <p> Duis autem vel eum iriure dolor in hendrerit in vulputate. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut. </p>
-                                    <p> Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                                        Brunch 3 wolf moon tempor. </p>
-                                    <p> Duis autem vel eum iriure dolor in hendrerit in vulputate. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut. </p>
-                                    <p>
-                                        <a class="btn red" href="ui_tabs_accordions_navs.html#collapse_3_4" target="_blank"> Activate this section via URL </a>
-                                    </p>
-                                </div>
-                            </div>
+
+                    <div class="form-body">
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">{{$web->getLable('name')}} :</label>
+                        <div class="col-md-6 tooltip-wide">
+                            <input type="text" class="form-control" name="site_name"  value="{{$res->site_name}}" >
+                     
                         </div>
                     </div>
+
+                    <div class="form-group">
+                                                <label class="col-md-3 control-label">Meta Description:</label>
+                                                <div class="col-md-9">
+                                                    <textarea class="form-control" name="meta_description" id="txteditor">{{$res->meta_description}}</textarea>
+                                                    <span class="help-block"> {{$web->getLable('max')}} 255 {{$web->getLable('chars')}} </span>
+                                                </div>
+                                            </div>
+
+             <div class="form-group">
+                    <label class="col-md-3 control-label">{{$web->getLable('image')}}:
+                       
+                    </label>
+                    <div class="col-md-8">
+                   @if(!empty($res->site_logo))      
+                   <div class="fileinput fileinput-new" data-provides="fileinput">
+                       <div id="inputimage" class="fileinput-new thumbnail" style="max-width:200px;">
+                                <img src="{{$res->site_logo}}"  alt=""  id="image_preview"/> </div>
+                            <input id="image_link" name="link" type="hidden" value="{{$res->site_logo}}">
+                            <div>
+                            <a class="btn iframe-btn default" type="button" href="{{base_url('assets/filemanager/dialog.php?type=1&field_id=image_link&fldr=images')}}">{{$web->getLable('choose_photo')}}</a>
+
+                            <a class="btn red" type="button" id="removeimages"  href="javascript:;">{{$web->getLable('delete_photo')}}</a>
+                         </div>
+                     </div>
+                     @else
+                     <div class="fileinput fileinput-new" data-provides="fileinput">
+                       <div id="inputimage" >
+                                <img src="" style="display:none;" alt=""  id="image_preview"/> </div>
+                            <input id="image_link" name="link" type="hidden" value="">
+                            <div>
+                            <a class="btn iframe-btn default" type="button" href="{{base_url('assets/filemanager/dialog.php?type=1&field_id=image_link&fldr=images')}}">{{$web->getLable('choose_photo')}}</a>
+
+                            <a class="btn red" type="button" id="removeimages" style="display:none;" href="javascript:;">{{$web->getLable('delete_photo')}}</a>
+
+                         </div>
+                     </div>
+                     @endif
+                                                                           
+
+                    </div>
                 </div>
+
+               
+                    <div class="form-group">
+                        <label class="col-md-3 control-label">{{$web->getLable('language_switching')}} :
+                                            </label>
+                        <div class="col-md-8">
+                            <input type="checkbox" name="lang_switching" data-on-text="{{$web->getLable('on')}}" data-off-text="{{$web->getLable('off')}}" class="make-switch"
+                                @if($res->lang_switching==0) {{'checked'}}
+                            @endif data-on-color="success" data-off-color="danger">
+                        </div>
+                    </div>
+                    
+                                        
+                </div>
+
+                        <div class="panel-group accordion" id="accordion3">
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a class="accordion-toggle accordion-toggle-styled collapsed" data-toggle="collapse" data-parent="#accordion3" href="#collapse_3_2">SEO</a>
+                                    </h4>
+                                </div>
+                                <div id="collapse_3_2" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        <div class="form-body">
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label">Meta Title:</label>
+                                                <div class="col-md-9">
+                                                    <input type="text" class="form-control maxlength-handler" name="meta_title" maxlength="100" value="{{$res->meta_title}}">
+                                                    <span class="help-block"> {{$web->getLable('max')}} 100 {{$web->getLable('chars')}} </span>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label">Meta Keywords:</label>
+                                                <div class="col-md-9">
+                                                    <textarea class="form-control maxlength-handler" rows="8" name="meta_keywords" maxlength="1000">{{$res->meta_keywords}}</textarea>
+                                                    <span class="help-block"> {{$web->getLable('max')}} 1000 {{$web->getLable('chars')}} </span>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label">Meta Description:</label>
+                                                <div class="col-md-10">
+                                                    <textarea class="form-control maxlength-handler" rows="8" name="meta_description" maxlength="255">{{$res->meta_description}}</textarea>
+                                                    <span class="help-block"> {{$web->getLable('max')}} 255 {{$web->getLable('chars')}} </span>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-md-3 control-label">{{$web->getLable('snippet_preview')}}:</label>
+                                                <div class="col-md-9">
+                                                    <div class="portlet light portlet-fit bordered">
+                                                        <div class="portlet-title bordernone">
+                                                            <div class="caption">
+                                                                <div>
+                                                                    <h4 class="caption-subject bold seo-font-blue uppercase" id="seotitle"><span  id="seo-title"></span> | {{$web->getDatafields(SETTING,'meta_title',array('settings_id'=>1))}}</h4>
+                                                                </div>
+
+                                                                <div>
+                                                                    <span class="seo-font-green">{{base_url()}}</span><span id="seo-url"></span>
+                                                                </div>
+
+                                                                <div class="caption-seo font-grey-cascade"> {{$web->getDatafields(SETTING,'meta_description',array('settings_id'=>1))}}</div>
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
                 <div class="portlet-title">
 
@@ -115,7 +168,7 @@
                         <button class="btn btn-success" type="submit" name="save" value="1">
                                                 <i class="fa fa-check"></i> {{$web->getLable('save')}}</button>
                         <button class="btn btn-success" type="submit" name="save_continue" value="1">
-                                                <i class="fa fa-check-circle"></i> {{$web->getLable('save_continue')}}</button>
+                           <i class="fa fa-check-circle"></i> {{$web->getLable('save_continue')}}</button>
 
                     </div>
                 </div>
@@ -125,3 +178,17 @@
     </div>
 </div>
 @endsection
+
+@section('script')
+<script src="{{base_url('assets/global/plugins/bootstrap-maxlength/bootstrap-maxlength.min.js')}}" type="text/javascript"></script>
+<script>
+      $('.maxlength-handler').maxlength({
+            limitReachedClass: "label label-danger",
+            alwaysShow: true,
+            threshold: 5
+        });
+</script>
+ <script src="{{base_url('assets/plugin/ckeditor/ckeditor.js')}}" type="text/javascript"></script>
+ <script src="{{base_url('assets/pages/scripts/editor.js')}}" type="text/javascript"></script>
+
+@endsection   
---
+++ assets/pages/scripts/editor.js
@@ -0,0 +1,19 @@
+//<![CDATA[
+    CKEDITOR.replace( 'txteditor',{
+        toolbar: [
+                { name: 'clipboard', items: [ 'Undo', 'Redo' ] },
+                { name: 'styles', items: [ 'Styles', 'Format' ] },
+                { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ] },
+                { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote' ] },
+                { name: 'links', items: [ 'Link', 'Unlink' ] },
+                { name: 'insert', items: [ 'Image', 'EmbedSemantic', 'Table' ] },
+                { name: 'tools', items: [ 'Maximize' ] },
+                { name: 'editing', items: [ 'Scayt' ] }
+            ],
+              
+    // อยากกำหนดอะไรก็ใส่ที่นี่
+    filebrowserBrowseUrl : baseurl+'assets/filemanager/dialog.php?type=2&editor=ckeditor&fldr=images',
+	filebrowserUploadUrl : baseurl+'assets/filemanager/dialog.php?type=2&editor=ckeditor&fldr=images',
+	filebrowserImageBrowseUrl : baseurl+'assets/filemanager/dialog.php?type=1&editor=ckeditor&fldr=images'
+    } );
+    //]]>
